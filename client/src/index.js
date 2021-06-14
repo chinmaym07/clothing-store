21df0017c4cf7c13a4e72e797decab3442c128c6
chinmaym07 <b418020@iiit-bh.ac.in>
2021-06-14 19:26:41 +0530

    Added All files
    Signed-off-by: chinmaym07 <b418020@iiit-bh.ac.in>

--- client/src/index.js
+++ client/src/index.js
@@ -9,7 +9,6 @@ import {store , persistor} from './redux/store';
 
 import { PersistGate } from 'redux-persist/integration/react';
 
-
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
@@ -22,8 +21,8 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
+serviceWorker.register();
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
